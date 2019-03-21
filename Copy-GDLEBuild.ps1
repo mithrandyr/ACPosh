de0aac2c621cd022c5f93745890bf4098f6e6f31
mithrandyr <mithrandyr@users.noreply.github.com>
2019-03-20 22:50:09 -0400

    removing verbose switch

--- Copy-GDLEBuild.ps1
+++ Copy-GDLEBuild.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 [string[]]$filesToCopy = @("GDLELogging.conf","GDLEnhanced.exe","GDLEnhanced.pdb","libmysql.dll")
 
 foreach ($f in $filesToCopy) {
-    Join-Path -Path $BuildFolder -ChildPath $f | Copy-Item -Destination $ServerFolder -Verbose
+    Join-Path -Path $BuildFolder -ChildPath $f | Copy-Item -Destination $ServerFolder
 }
 
 $BuildDataFolder = Join-Path $BuildFolder "Data"
@@ -12,5 +12,5 @@ $ServerDataFolder = Join-Path $ServerFolder "Data"
 
 Get-ChildItem -Path $BuildDataFolder -Recurse -File -Name |
     ForEach-Object {
-        Copy-Item -Path (Join-Path $BuildDataFolder $_) -Destination (Join-Path $ServerDataFolder $_) -Force -Verbose
+        Copy-Item -Path (Join-Path $BuildDataFolder $_) -Destination (Join-Path $ServerDataFolder $_) -Force
     }
