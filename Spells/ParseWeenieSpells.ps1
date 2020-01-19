6f82ff4d3a65e595cf8c78e05e628db1001c0579
mithrandyr <mithrandyr@users.noreply.github.com>
2020-01-18 21:37:58 -0500

    more stuff

---
+++ Spells/ParseWeenieSpells.ps1
@@ -0,0 +1,13 @@
+param([parameter(Mandatory)][string]$filePath, [switch]$AsString)
+
+$weenie = Get-Content -Raw -Path $filePath | ConvertFrom-Json
+$spells = Get-Content -Raw -Path "$PSScriptRoot\spellData.json" | ConvertFrom-Json
+
+$results = $weenie.spellbook.key |
+    ForEach-Object { 
+        $id = $_
+        $spells.where({$_.SpellId -eq $id})
+    } | Sort-Object School, Category
+
+if($AsString) { $results | ForEach-Object { "{0} - {1} [{2} : {3}] ({4}) > {5}" -f $_.SpellId, $_.Name, $_.School, $_.SpellLevel, $_.Category, $_.Description } }
+else { $results }
--- Spells/SearchSpells.ps1
+++ Spells/SearchSpells.ps1
@@ -1,4 +1,39 @@
-param([string]$filePath = "$PSScriptRoot\spellData.json")
+param([string]$Name
+        , [string[]][ValidateSet("Life Magic","Item Enchantment","Creature Enchantment","War Magic","Void Magic")]$School
+        , [int[]][ValidateSet(1,2,3,4,5,6,7,8)]$SpellLevel
+        , [string[]][ValidateSet("Resistable","PKSensitive","Beneficial", "TargetSelf","TargetOther","Reversed","NotIndoor","NotResearchable","FastCast")]$Flags
+        , [string]$filePath = "$PSScriptRoot\spellData.json"
+        , [switch]$ForClipboard
+    )
 
 $spells = Get-Content $filePath -Raw | ConvertFrom-Json
 
+$filterArray = @()
+if($name) { $filterArray += {$_.Name -like "*$name*"} }
+if($School) { $filterArray += {$_.School -in $School} }
+if($SpellLevel) { $filterArray += {$_.SpellLevel -in $SpellLevel} }
+if($Flags) { $filterArray += {
+    $r = $false
+    foreach($f in $Flags) {
+        if($_.Flags -contains $f) { $r = $f -and $true }
+        else {
+            $r = $false
+            break
+        }
+    }
+    $r
+} }
+
+foreach($f in $filterArray) {
+    $spells = $spells | Where-Object $f
+}
+
+$spells = $spells | Sort-Object School, @{Expression = "SpellLevel"; Descending= $True}, Category, Name
+
+if($ForClipboard) {
+    $spells |
+        Out-GridView -Title "Select the Spells you want..." -OutputMode Multiple |
+        ForEach-Object { ', { "key": ' + $_.SpellId.ToString() + ', "value": { "casting_likelihood": 2.0 } }' } |
+        Set-Clipboard
+}
+else { $spells }
