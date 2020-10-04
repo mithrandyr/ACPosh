ad3f36b96c56a50a7d815daf3df575e6a93d4e90
mithrandyr <mithrandyr@users.noreply.github.com>
2020-10-03 21:33:13 -0400

    useful

--- Customizations/IncreaseDropRates.ps1
+++ Customizations/IncreaseDropRates.ps1
@@ -6,78 +6,92 @@ param([parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)][A
 begin {
     if($ItemDropRates.Count -eq 0) {
         $ItemDropRates = @{
-            6353 = .1 #Pyreal Mote
-            6355 = .1 #Pyreal SLiver
-            6060 = .1 #Dark Speck
-            6059 = .1 #Dark Sliver
-            6058 = .1 #Dark Shard
-            6055 = .1 #Cracked Shard
-            6057 = .1 #Tiny Shard
-            6056 = .1 #Small Shard
+            6353 = .2 #Pyreal Mote
+            6355 = .1 #Pyreal Sliver
+            6060 = .2 #Dark Speck
+            6059 = .2 #Dark Sliver
+            6058 = .2 #Dark Shard
+            6055 = .2 #Cracked Shard
+            6057 = .2 #Tiny Shard
+            6056 = .2 #Small Shard
         }
     }
     if(-not(Test-Path -Path $ChangedInfoDirectory)) { New-Item -ItemType Directory -Path $ChangedInfoDirectory | Out-Null }
     $ErrorActionPreference = "stop"
+
+    $FileList = [System.Collections.Generic.List[string]]::new()
+    $fileCount = 0
 }
 
-process {
-    try {
-        $data = Get-Content -Path $path -Raw | ConvertFrom-Json
+process { 
+    $FileList.Add($Path)
+    $fileCount += 1
+}
+end {
+    $fCount = $FileList.Count
+    $fProcessed = 0
+    foreach($file in $FileList) {
+        $fProcessed += 1
+        Write-Progress -Activity "IncreasedDropRate Change" -Status $file -PercentComplete ($fProcessed * 100 / $fCount)
+        try {
+            $data = Get-Content -Path $file -Raw | ConvertFrom-Json
+                    
+            if($data.weenieType -eq 10) {
+                $changeData = [PSCustomObject]@{
+                    FileName = $file
+                    WCID = $data.wcid
+                    Changes = @()
+                }            
                 
-        if($data.weenieType -eq 10) {
-            $changeData = [PSCustomObject]@{
-                FileName = $Path
-                WCID = $data.wcid
-                Changes = @()
-            }            
-            
-            $remainingRate = 1.0
-            $madeChange = $false
-            foreach($t in $data.createList) {
-                $treasureWCID = $t.wcid
-                if($treasureWCID -in $ItemDropRates.Keys) {
-                    $newRate = $ItemDropRates.$treasureWCID #Get dropRate
-                    if($newRate -gt $t.shade) { #see if droprate is greater than existing
-                        $changeData.Changes += [PSCustomObject]@{
-                            wcid = $t.wcid
-                            OriginalRate = $t.shade
-                            NewRate = $newRate
+                $remainingRate = 1.0
+                $madeChange = $false
+                foreach($t in $data.createList) {
+                    $treasureWCID = [int]$t.wcid
+                    if($treasureWCID -in $ItemDropRates.Keys) {
+                        $newRate = $ItemDropRates[$treasureWCID] #Get dropRate
+                        if($newRate -gt $t.shade) { #see if droprate is greater than existing
+                            $changeData.Changes += [PSCustomObject]@{
+                                wcid = $t.wcid
+                                OriginalRate = $t.shade
+                                NewRate = $newRate
+                            }
+                            $t.shade = $newRate
+                            $madeChange = $true
+                            $remainingRate -= $newRate
                         }
-                        $t.shade = $newRate
-                        $madeChange = $true
-                        $remainingRate -= $newRate
                     }
-                }
-                elseif($t.wcid -eq 0) {
-                    if($madeChange) {
-                        $t.shade = $remainingRate
-                        $madeChange = $false
+                    elseif($t.wcid -eq 0) {
+                        if($madeChange) {
+                            $t.shade = $remainingRate
+                            $madeChange = $false
+                        }
+                        $remainingRate = 1.0
                     }
-                    $remainingRate = 1.0
+                    else { $remainingRate -= $d.shade }
                 }
-                else { $remainingRate -= $d.shade }
-            }
 
-            if($changeData.Changes.Count -gt 0) {
-                $changeData |
-                    ConvertTo-Json -Compress |
-                    Set-Content -Path (Join-Path $ChangedInfoDirectory ("changed-{0}-{1:yyyyMMdd}.json" -f $changeData.wcid, (get-date)))
-                
-                if(-not $noChange){
-                    $data |
-                    ConvertTo-Json -Compress -Depth 10 | 
-                    Set-Content -Path $Path
-                }
-                else {
-                    $data |
-                    ConvertTo-Json -Compress -Depth 10 | 
-                    Set-Content -Path (join-Path $ChangedInfoDirectory (Split-Path $path -Leaf))
-                }
-            }            
+                if($changeData.Changes.Count -gt 0) {
+                    $changeData |
+                        ConvertTo-Json -Compress |
+                        Set-Content -Path (Join-Path $ChangedInfoDirectory ("changed-{0}-{1:yyyyMMdd}.json" -f $changeData.wcid, (get-date)))
+                    
+                    if(-not $noChange){
+                        $data |
+                        ConvertTo-Json -Compress -Depth 10 | 
+                        Set-Content -Path $file
+                    }
+                    else {
+                        $data |
+                        ConvertTo-Json -Compress -Depth 10 | 
+                        Set-Content -Path (join-Path $ChangedInfoDirectory (Split-Path $file -Leaf))
+                    }
+                }            
+            }
+        }
+        catch {
+            Write-Warning "Could not process: $file"
+            $_
         }
     }
-    catch {
-        Write-Warning "Could not process: $path"
-        $_
-    }
+    Write-Progress -Activity "IncreasedDropRate Change" -Completed
 }
