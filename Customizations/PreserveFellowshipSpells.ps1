2d7f78f06a73af991f2809bbb512f17a8ee3f0e4
mithrandyr <mithrandyr@users.noreply.github.com>
2022-02-03 18:49:14 -0500

    fixes

--- Customizations/PreserveFellowshipSpells.ps1
+++ Customizations/PreserveFellowshipSpells.ps1
@@ -8,4 +8,4 @@ $spells = Get-Content -Raw -Path $PathToSpellsJson | ConvertFrom-Json
 $spellList = $spells.table.spellBaseHash | Where-Object key -NotIn $preservedFellowship.key
 $spellList += $preservedFellowship
 $spells.table.spellBaseHash = $spellList | Sort-Object key
-$spells | ConvertTo-Json -Depth 15 -Compress | Set-Content -Path $PathToSpellsJson
+$spells | ConvertTo-Json -Depth 15 -Compress | Set-Content -Path $PathToSpellsJson -Force
